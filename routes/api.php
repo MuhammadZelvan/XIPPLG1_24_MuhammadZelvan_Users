0e75c582dbc07a72eb59fe366c365d6a0b6bad1d
MuhammadZelvan <muhammadzelvan263@gmail.com>
2025-02-12 09:50:58 +0700

    Book API

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoanController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\BookController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +16,4 @@ Route::apiResource('categories', CategoryController::class);
 Route::apiResource('user', UserController::class);
 Route::apiResource('loans', LoanController::class);
 Route::apiResource('reviews', ReviewController::class);
+Route::apiResource('book', BookController::class);
